f1b8ed2a311780d2406e3d2062dce0f88c6fbc9f
Micah Woodring <micahkwoodring@gmail.com>
2025-03-31 12:29:07 -0700

    Starting main opening section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import logo from "../../svg/logo.svg";
 import github from "../../svg/github.svg";
 import linkedin from "../../svg/linkedin.svg";
+//import portrait from "../../images/portrait.JPG";
 
 export default function HomePage() {
   const router = useRouter();
@@ -16,13 +17,19 @@ export default function HomePage() {
       <header className="h-18">
         <div className="grid grid-cols-3 h-full">
           <section className="flex items-center justify-evenly">
-            <Image src={logo} alt="Logo" width={40} height={40} />
+            <Image
+              src={logo}
+              alt="Logo"
+              width={40}
+              height={40}
+              style={{ filter: "invert(0.2)" }}
+            />
             <div></div>
             <div></div>
           </section>
           <nav className="flex items-center justify-evenly">
             <div className="w-[70%] h-4/6 border-1 rounded-4xl border-gray-400">
-              <ul className="flex items-center justify-evenly h-full">
+              <ul className="flex items-center justify-evenly h-full flex-wrap">
                 <li>
                   <Link href={"/home"}>
                     <h1 className="text-gray-500 hover:text-gray-700">Home</h1>
@@ -50,19 +57,45 @@ export default function HomePage() {
               </ul>
             </div>
           </nav>
-          <section className="flex items-center justify-evenly">
-            <div className="w-[25%] h-1/2 bg-gray-700 rounded-4xl flex flex-row items-center justify-evenly">
+          <section className="flex items-center justify-evenly flex-wrap">
+            <div></div>
+            <div></div>
+            <div className="w-[20%] h-1/2 bg-gray-700 rounded-4xl flex flex-row items-center justify-evenly">
               <Link
                 href="https://github.com/EliteMicah/SWE-Over-Under"
                 target="_blank"
               >
-                <Image src={github} alt="github" width={25} height={25} />
+                <Image
+                  src={github}
+                  alt="github"
+                  width={23}
+                  height={23}
+                  style={{ filter: "invert(0.2)" }}
+                  className="hover:!filter-[invert(0.1)]"
+                />
               </Link>
-              <Image src={linkedin} alt="linkedin" width={25} height={25} />
+              <Link
+                href="https://www.linkedin.com/in/software-engineer-micahwoodring/"
+                target="_blank"
+              >
+                <Image
+                  src={linkedin}
+                  alt="linkedin"
+                  width={23}
+                  height={23}
+                  style={{ filter: "invert(0.2)" }}
+                  className="hover:!filter-[invert(0.1)]"
+                />
+              </Link>
             </div>
           </section>
         </div>
       </header>
+      <section className="flex justify-center">
+        <div className="w-[90%] border-2 border-gray-300 items-center h-screen rounded-2xl">
+          <div></div>
+        </div>
+      </section>
     </div>
   );
 }
